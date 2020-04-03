82778c8564b84cffea19b82cab7f535f86607d12
Ammar Najjar <najjarammar@protonmail.com>
2020-04-03 23:41:11 +0200

    feat: add aiohttp example

---
+++ http_requests/http_client.py
@@ -0,0 +1,24 @@
+""" aiohttp client example
+
+TODO: https://docs.aiohttp.org/en/stable/testing.html
+
+"""
+import asyncio
+
+import aiohttp
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'https://python.org')
+        print(html)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
