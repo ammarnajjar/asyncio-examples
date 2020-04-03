9ddf9dddc85350f5a39d6681560253bd2bc51e9c
Ammar Najjar <najjarammar@protonmail.com>
2020-04-03 02:34:56 +0200

    test: hello.say

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 import asyncio
 
 
+async def print_task(task):
+    print(await task)
+
+
 async def say(what, when):
     await asyncio.sleep(when)
-    print(what)
+    return(what)
 
 
 async def stop_after(loop, when):
@@ -12,10 +16,9 @@ async def stop_after(loop, when):
 
 loop = asyncio.get_event_loop()
 
-loop.create_task(say('hello', 2))
-loop.create_task(say('world', 1))
+loop.create_task(print_task(say('hello', 2)))
+loop.create_task(print_task(say('world', 1)))
 loop.create_task(stop_after(loop, 3))
-
 
 loop.run_forever()
 loop.close()
---
+++ test_hello.py
@@ -0,0 +1,8 @@
+import pytest
+from hello import say
+
+
+@pytest.mark.asyncio
+async def test_hello():
+    res = await say('hello', 0)
+    assert 'hello' == res
